ed674414981fc09221eb6bd36f920828e3e9f8a8
kevinekarevano <kev1n.ek4revano@gmail.com>
2025-06-17 21:33:46 +0700

    fix: Enhance layout styling for better responsiveness and overflow handling

--- client/src/components/layouts/DashboardLayout.jsx
+++ client/src/components/layouts/DashboardLayout.jsx
@@ -7,14 +7,14 @@ import CustomTrigger from "./sidebar/customTrigger";
 export default function Layout() {
   return (
     <>
-      <SidebarProvider className="bg-[#232E30] font-poppins ">
+      <SidebarProvider className="bg-[#232E30]  overflow-x-hidden font-poppins ">
         <AppSidebar />
-        <main className="w-full">
+        <main className="w-full max-w-full overflow-auto">
           <DashboardNavbar />
 
           <CustomTrigger />
 
-          <div className="p-5">
+          <div className="p-5  mb-50">
             <Outlet />
           </div>
         </main>
